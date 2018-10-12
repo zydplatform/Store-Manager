0974c491c1b11c201de018e413784b3278d1ba7c
Kiganda Ivan <kigandaivan32@gmail.com>
2018-10-12 21:57:10 +0300

    more changes to files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Store-Manager
 Store manager application for single store
+This application's User Interface is built only 
+in Html , css and javascript 
---
+++ login.js
@@ -0,0 +1,7 @@
+function UserLogin(){
+    var username=document.getElementById('user').value;
+    var password=document.getElementById('password').value;
+    if(username =="" && password ==""){
+        window.alert('username and password!');
+    }
+}
