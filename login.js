c34e4e2be918a2b9ac479482efad1fc6ffd22456
Kiganda Ivan <kigandaivan32@gmail.com>
2018-10-13 12:48:56 +0300

    new changes

--- login.js
+++ login.js
@@ -4,4 +4,8 @@ function UserLogin(){
     if(username =="" && password ==""){
         window.alert('username and password!');
     }
+    else if(username.value=='admin' && password.value=='admin'){
+        window.alert('welcome to Admin Dashboard');
+        
+    }
 }
